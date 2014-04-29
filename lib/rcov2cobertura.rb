3b768fb22f6713ed3654cd05393be10af5b99d50
rohan <rohan@mcgovern.id.au>
2014-04-29 21:26:51 +1000

    Added basically working formatter.
    Sample usage:
    
      ruby -Ilib -rrcov2cobertura `which rcov` --custom-formatter 'Rcov2Cobertura::Formatter' ./foo.rb
    
    ...creates coverage/cobertura.xml along with the usual rcov outputs.

---
+++ lib/rcov2cobertura.rb
@@ -0,0 +1,5 @@
+module Rcov2Cobertura
+  VERSION = '1.0.0'
+end
+
+require 'rcov2cobertura/formatter'
---
+++ lib/rcov2cobertura/formatter.rb
@@ -0,0 +1,84 @@
+require 'erb'
+
+module Rcov2Cobertura
+  class Formatter
+    TEMPLATE_FILE = File.join(File.dirname(File.expand_path(__FILE__)), 'coverage.xml.erb')
+
+    def initialize(args={})
+      @destdir = args[:destdir] || 'coverage'
+      @ignore = args[:ignore] || []
+      @dont_ignore = args[:dont_ignore] || []
+      @covdata = []
+      @total_loc = 0
+      @total_covered_loc = 0
+    end
+
+    def add_file(filename, lines, covered, counts)
+      return if should_ignore?(filename)
+
+      loc = lines.count{|l| is_loc?(l)}
+      @total_loc += loc
+
+      # fixup covered for 'end'.
+      # This is a heuristic seemingly used by rcov:
+      # for an 'end' line, count the line as covered iff the previous line was covered.
+      covered.each_with_index do |c,idx|
+        next if idx == 0
+        next if c
+        if covered[idx-1] && lines[idx] =~ /^\s*end\s*(#.*)?$/
+          covered[idx] = true
+          counts[idx] = counts[idx-1]
+          if counts[idx] == 0
+            counts[idx] = 1
+          end
+        end
+      end
+
+      i = 0
+      covered_loc = covered.count do |c|
+        i += 1
+        c && is_loc?(lines[i-1])
+      end
+      @total_covered_loc += covered_loc
+
+      @covdata << {
+        :name => filename,
+        :lines => lines,
+        :covered => covered,
+        :counts => counts,
+        :branch_rate => 1.0,
+        :line_rate => covered_loc.to_f / loc
+      }
+    end
+
+    def execute
+      src = IO.read TEMPLATE_FILE
+      erb = ERB.new(src, nil, '%-<>')
+      erb.filename = TEMPLATE_FILE
+      out = erb.result(get_binding)
+
+      File.open(File.join(@destdir, 'cobertura.xml'), 'w') do |f|
+        f.write out
+      end
+    end
+
+    private
+
+    def should_ignore?(file)
+      @ignore.any?{|rx| file =~ rx} && !@dont_ignore.any?{|rx| file =~ rx}
+    end
+
+    # FIXME: just a hueristic, doesn't quite seem right.
+    # Compare with rcov.
+    def is_loc?(line)
+      line !~ /^\s*(#.*)?$/
+    end
+    
+    def get_binding
+      branch_rate = 1.0
+      line_rate = @total_covered_loc.to_f / @total_loc
+      covered_files = @covdata
+      binding()
+    end
+  end
+end
